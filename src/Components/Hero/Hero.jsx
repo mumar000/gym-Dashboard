5493477faf04f1499022773737ee9d3f1bf3cd0c
Muhammad Umar <muhammad-umar@umarabdulhaque@gmail.com>
2025-02-23 09:16:37 +0500

    changes in the header

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import {Button } from '../components';
 
 const Hero = () => {
   return (
     <div
-      className="flex items-center justify-center h-screen" // Added h-screen for full height
+      className="flex flex-col gap-4 items-center justify-center h-screen"
       style={{
-        backgroundImage: "url('https://images.unsplash.com/photo-1581009146145-b5ef050c2e1e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1581009146145-b5ef050c2e1e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         backgroundSize: 'cover',
-        backgroundPosition: 'center', // Optional: Center the background image
+        backgroundPosition: 'center',
       }}
     >
-      <h1 className="text-4xl font-bold text-white text-center">
-        Welcome to Commando Gym
+      <div className=" items-center justify-center text-center ">
+      <h1 className="text-5xl font-bold text-gray-100 md:text-5xl lg:text-6xl drop-shadow-lg">
+        Welcome to <span className="text-green-500">Commando Gym</span>
       </h1>
+
+      </div>
+      <Button />
     </div>
   );
 };
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Dashboard() {
   return (
     <div>
-    <nav className=" bg-white  shadow-md ">
+    <nav className=" bg-white  shadow-md rounded-lg z-10 ">
       <div className="container mx-auto flex items-center justify-center p-4">
         {/* Logo */}
         <Link to='/'>
@@ -14,12 +14,12 @@ export default function Dashboard() {
 
         {/* Navigation Links */}
         <div className="flex  items-center text-gray-800   space-x-6">
-          {/* <Link 
+          <Link 
             to="/form" 
             className="text-lg font-medium  transition ease duration-200"
           >
             Add a Customer
-          </Link> */}
+          </Link>
           <Link 
             to="/details" 
             className="text-lg font-medium   transition ease   duration-300"
